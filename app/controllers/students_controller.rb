2f82fc81589ad80aba7ab06932b563f847d487a0
Stephanie Chandra <stephaniechandra@gmail.com>
2014-03-07 15:35:23 -0500

    updated index

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,7 +7,7 @@ class StudentsController < ApplicationController
     # Load all the students into an instance variable.
     # We use the ::all method on the Student class, provided by Sequel
     @students = Student.all
-    erb :'students/index' # render the index.erb within app/views/students
+    erb :'students/index.html' # render the index.erb within app/views/students
   end
 
   # Build the rest of the routes here.

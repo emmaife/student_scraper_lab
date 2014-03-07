05f84aff0756a037a44a95d3ee354ca0bf3ed3d2
amcaplan <ariel.caplan@mail.yu.edu>
2014-03-07 12:07:03 -0500

    Create student migration

---
+++ db/migrate/01_create_students.rb
@@ -0,0 +1,18 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :profile_image
+      t.string :background_image
+      t.string :twitter
+      t.string :linkedin
+      t.string :github
+      t.string :quote
+      t.string :bio
+      t.string :work
+      t.string :work_title
+      t.string :education
+      t.timestamps
+    end
+  end
+end
